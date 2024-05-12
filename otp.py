dc5989a5499c16481d49fcfa52b677dd66150f3d
saroshirfan2002 <166515100+saroshirfan2002@users.noreply.github.com>
2024-05-13 00:58:40 +0500

    Update otp.py

--- otp.py
+++ otp.py
@@ -15,7 +15,7 @@ def generate_string(length):
 
   return verif_code
 
-def otp_msg(attendance):
+def otp_msg():
   my_email = 'sendsystem8@gmail.com'
   password_key = 'ydgw mibv qeon pzcn'
   nu_server = "smtp.gmail.com"
@@ -32,59 +32,55 @@ def otp_msg(attendance):
       flag = 0
     else :
       flag = 1
-  id_new = id[1] + id[2] + id[0] + id[3:8]
-  if id_new in attendance:
-    return 101
-  else:
-    parts = id.split('-'id_new = id[1] + id[2] + id[0] + id[3:8])
-    recipient_email = parts[0] + parts[1] +'@nu.edu.pk'
-    print(recipient_email)
-    code = generate_string(8)
-    # HTML content with a bold heading and bigger font size
-    html_content = f"""
-    <html>
-      <head></head>
-      <body>
-        <p style="font-size: 16px;">Hey {id}!</p>
-        <h1 style="font-size: 24px; font-weight: bold;">Your Verificaion Code is {code} </h1>
-        <p style="font-size: 16px;">Enter this verification code, to mark your attendance.</p>
-        <p style="font-size: 16px;">In case of any query, please contact your student affairs.</p>
-        <br>
-        <p style="font-size: 16px;">Regards,</p>
-        <p style="font-size: 16px;">team</p>
-      </body>
-    </html>
-    """
-  
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Verification Code'
-    msg['From'] = my_email
-    msg['To'] = recipient_email  # Update with the recipient's email
-  
-    # Create a MIMEText object with the HTML content and attach it to the message
-    body = MIMEText(html_content, 'html')
-    msg.attach(body)
-  
-    with smtplib.SMTP(nu_server, nu_port) as my_server:
-        my_server.ehlo()
-        my_server.starttls()
-        my_server.login(my_email, password_key)
-        my_server.send_message(msg)
-  
-    print('Email sent successfully!')
-  
-    print(code)
-    entered_code = ''
-    start_time = time.time()
-    while time.time() - start_time < 20 and len(entered_code ) <= 0:
-      #PLEASE ENTER YOUR OTP
-      entered_code = simpledialog.askstring("Input", "Enter OTP:")
-      if entered_code == code:
-        #YOUR ATTENDANCE HAS BEEN MARKED USING OTP
-        print('verifing...')
-        return id_new
-      else:
-        #INCORRECT OTP: YOUR ATTENDANCE IS NOT MARKED
-        print('code expired')
-        return False
+  parts = id.split('-'id_new = id[1] + id[2] + id[0] + id[3:8])
+  recipient_email = parts[0] + parts[1] +'@nu.edu.pk'
+  print(recipient_email)
+  code = generate_string(8)
+  # HTML content with a bold heading and bigger font size
+  html_content = f"""
+  <html>
+    <head></head>
+    <body>
+      <p style="font-size: 16px;">Hey {id}!</p>
+      <h1 style="font-size: 24px; font-weight: bold;">Your Verificaion Code is {code} </h1>
+      <p style="font-size: 16px;">Enter this verification code, to mark your attendance.</p>
+      <p style="font-size: 16px;">In case of any query, please contact your student affairs.</p>
+      <br>
+      <p style="font-size: 16px;">Regards,</p>
+      <p style="font-size: 16px;">team</p>
+    </body>
+  </html>
+  """
+
+  msg = MIMEMultipart()
+  msg['Subject'] = 'Verification Code'
+  msg['From'] = my_email
+  msg['To'] = recipient_email  # Update with the recipient's email
+
+  # Create a MIMEText object with the HTML content and attach it to the message
+  body = MIMEText(html_content, 'html')
+  msg.attach(body)
+
+  with smtplib.SMTP(nu_server, nu_port) as my_server:
+      my_server.ehlo()
+      my_server.starttls()
+      my_server.login(my_email, password_key)
+      my_server.send_message(msg)
+
+  print('Email sent successfully!')
+
+  print(code)
+  entered_code = ''
+  start_time = time.time()
+  while time.time() - start_time < 20 and len(entered_code ) <= 0:
+    #PLEASE ENTER YOUR OTP
+    entered_code = simpledialog.askstring("Input", "Enter OTP:")
+    if entered_code == code:
+      #YOUR ATTENDANCE HAS BEEN MARKED USING OTP
+      print('verifing...')
+      return id_new
+    else:
+      #INCORRECT OTP: YOUR ATTENDANCE IS NOT MARKED
+      print('code expired')
+      return False
 
